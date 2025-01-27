6de1dbc98086041e5f86036de596112d318eb950
aarsyah0 <arsasaja2@gmail.com>
2025-01-27 19:14:45 +0700

    Arsyah_backend_membuat routing

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProfileController;
+use App\Http\Controllers\API\TodoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('user/profile', [ProfileController::class, 'getProfile']);
+    Route::post('user/profile', [ProfileController::class, 'updateProfile']);
+
+
+    Route::get('todos', [TodoController::class, 'index']);
+    Route::post('todos', [TodoController::class, 'store']);
+    Route::put('todos/{id}', [TodoController::class, 'update']);
+    Route::delete('todos/{id}', [TodoController::class, 'destroy']);
 });
