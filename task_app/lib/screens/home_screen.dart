756115fca333dd5e52239bb9de20b09a68262237
gasti21 <gasti.hisyam@gmail.com>
2025-01-29 16:19:57 +0700

    ghasti_frontend perbaikan dan pembuatan home

--- task_app/lib/screens/home_screen.dart
+++ task_app/lib/screens/home_screen.dart
@@ -1,1 +1,345 @@
+import 'package:flutter/material.dart';
 
+import '../models/todo.dart';
+import '../screens/profile_screen.dart';
+import '../services/todo_service.dart';
+
+class HomeScreen extends StatefulWidget {
+  final String token;
+  HomeScreen({required this.token});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late TodoService _todoService;
+  List<Todo> _todos = [];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _todoService = TodoService(widget.token);
+    _loadTodos();
+  }
+
+  Future<void> _loadTodos() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      List<Todo> todos = await _todoService.getTodos();
+      setState(() {
+        _todos = todos;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Error: $e')));
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _addTodo() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        final TextEditingController titleController = TextEditingController();
+        final TextEditingController descriptionController =
+            TextEditingController();
+
+        return AlertDialog(
+          title: Text('Add Todo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: titleController,
+                decoration: InputDecoration(labelText: 'Title'),
+              ),
+              TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(labelText: 'Description'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Cancel')),
+            TextButton(
+              onPressed: () async {
+                final title = titleController.text;
+                final description = descriptionController.text;
+
+                if (title.isNotEmpty) {
+                  await _todoService.addTodo(title, description);
+                  _loadTodos();
+                  Navigator.of(context).pop();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Title cannot be empty')));
+                }
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _updateTodo(int id, String title, String description, bool isCompleted) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        final TextEditingController titleController =
+            TextEditingController(text: title);
+        final TextEditingController descriptionController =
+            TextEditingController(text: description);
+        bool updatedIsCompleted = isCompleted;
+
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Update Todo'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(labelText: 'Description'),
+                  ),
+                  CheckboxListTile(
+                    title: Text('Completed'),
+                    value: updatedIsCompleted,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        updatedIsCompleted = value ?? false;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final updatedTitle = titleController.text;
+                    final updatedDescription = descriptionController.text;
+
+                    if (updatedTitle.isNotEmpty) {
+                      await _todoService.updateTodo(
+                        id,
+                        updatedTitle,
+                        updatedDescription,
+                        updatedIsCompleted,
+                      );
+                      _loadTodos();
+                      Navigator.of(context).pop();
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Title cannot be empty')),
+                      );
+                    }
+                  },
+                  child: Text('Update'),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    bool? confirm = await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Confirm Delete'),
+                          content: Text(
+                              'Are you sure you want to delete this todo?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(false),
+                              child: Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(true),
+                              child: Text('Delete'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+
+                    if (confirm == true) {
+                      await _todoService.deleteTodo(id);
+                      _loadTodos();
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: Text(
+                    'Delete',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _deleteTodoWithConfirmation(int id) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Delete Todo'),
+          content: Text('Are you sure you want to delete this todo?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                await _todoService.deleteTodo(id);
+                _loadTodos();
+                Navigator.of(context).pop();
+              },
+              child: Text('Delete'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Todo> onGoingTodos =
+        _todos.where((todo) => !todo.isCompleted).toList();
+    List<Todo> completedTodos =
+        _todos.where((todo) => todo.isCompleted).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProfileScreen(token: widget.token),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // On Going Section
+                  if (onGoingTodos.isNotEmpty) ...[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'On Going',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    ListView.builder(
+                      itemCount: onGoingTodos.length,
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        Todo todo = onGoingTodos[index];
+                        return ListTile(
+                          title: Text(todo.title),
+                          subtitle: Text(todo.description),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () => _updateTodo(
+                                    todo.id,
+                                    todo.title,
+                                    todo.description,
+                                    todo.isCompleted),
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () =>
+                                    _deleteTodoWithConfirmation(todo.id),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                  if (completedTodos.isNotEmpty) ...[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Done',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    ListView.builder(
+                      itemCount: completedTodos.length,
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        Todo todo = completedTodos[index];
+                        return ListTile(
+                          title: Text(
+                            todo.title,
+                            style: TextStyle(
+                              decoration: TextDecoration.lineThrough,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          subtitle: Text(todo.description),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete, color: Colors.red),
+                            onPressed: () =>
+                                _deleteTodoWithConfirmation(todo.id),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ],
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
